62e6d7a2e4c397872dde4dcb398b5f493b455fe8
luisa1061278175 <luisafeosorio@gmail.com>
2024-03-25 16:00:41 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module co.edu.uniquindio.reservasevento {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.reservasevento to javafx.fxml;
+//    exports co.edu.uniquindio.reservasevento;
+//    exports co.edu.uniquindio.reservasevento.controller;
+//    exports co.edu.uniquindio.reservasevento.exceptions;
+//    exports co.edu.uniquindio.reservasevento.model;
+//    exports co.edu.uniquindio.reservasevento.utils;
+//    exports co.edu.uniquindio.reservasevento.viewController;
+
+
+
+
+}
